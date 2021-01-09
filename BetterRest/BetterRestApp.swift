60eea8f970078a24db3c5281fad9891526c2eec5
Patricia <patysiq@yahoo.com.br>
2021-01-09 16:41:52 -0300

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by PATRICIA S SIQUEIRA on 09/01/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
